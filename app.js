3c8680ded310e8025a01ade07a6e20f40522cb23
Rj <rj.bernaldo@gmail.com>
2014-08-17 13:55:48 -0700

    Adds session views

--- app.js
+++ app.js
@@ -5,12 +5,19 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var passport = require('passport');
+var flash = require('flash');
+var morgan = require('morgan');
+var session = require('session');
 var routes = require('./routes/index');
-var users = require('./routes/users');
+
+
+var configDB = require('./config/database.js');
+
+mongoose.connect(configDB.url);
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -21,20 +28,19 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use(session({ secret: 'test '}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
-/// catch 404 and forward to error handler
+app.use('/', routes);
+
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
-/// error handlers
-
-// development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
@@ -45,8 +51,6 @@ if (app.get('env') === 'development') {
     });
 }
 
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/login', function(req, res) {
+  res.render('login', { message: req.flash('loginMessage') });
+});
+
+router.get('/signup', function(req, res) {
+  res.render('signup', { message: req.flash('signupMessage') })
+});
+
+router.get('/profile', function(req, res) {
+  res.render('profile', { user: req.user });
+});
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    res.redirect('/');
+  }
+}
 
 module.exports = router;
