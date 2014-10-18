56ae577312dc0d9ffb0f66a1f85405c6b06017a0
Rj <rj.bernaldo@gmail.com>
2014-10-18 14:47:10 -0700

    Changes model

--- config/auth.js
+++ config/auth.js
@@ -5,5 +5,6 @@ module.exports = {
     'accessTokenSecret': 'boFHQsl8Uy3exNoAiFjAcmS3UFTuTO9FvRt6eWHEB70Dg',
     'callbackURL': '/auth/twitter/callback',
     'accessToken': '2798340624-daJn9ajfvVtTb9jzvtBYXlRLSY1c5HaJB0rtzYx'
+    
   }
 }
